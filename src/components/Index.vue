3b0e71a4393d007a703feffc38f8571fdada7aa4
TL-ai <tanglin_zhou@163.com>
2020-04-01 17:34:29 +0800

    xx

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -143,6 +143,7 @@
                 </el-carousel> -->
             </el-row>
             <el-row>
+                
                 <el-button type="text" @click="dialogFormVisible = true">打开嵌套表单的 Dialog</el-button>
 
                 <el-dialog title="收货地址" :visible.sync="dialogFormVisible">
@@ -153,7 +154,6 @@
                         <el-form-item label="活动区域" :label-width="formLabelWidth">
                             <el-select v-model="form.region" placeholder="请选择活动区域">
                                 <el-option label="区域一" value="shanghai"></el-option>
-                                <el-option label="区域二" value="beijing"></el-option>
                             </el-select>
                         </el-form-item>
                     </el-form>
