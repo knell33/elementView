291b59443d2055978efebb999c9af1a4c5ac1540
佳 刘 <2044432166@qq.com>
2020-04-08 11:49:57 +0800

    4月8日 LHY

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -3,12 +3,20 @@
     <el-container>
         <!-- 资源目录树形表格 -->
         <el-aside width="350px">
-            <el-table :data="ResourceTableData" style="width: 100%;margin-bottom: 20px;" height="1000px" row-key="资源名称" border :tree-props="{children: 'children', hasChildren: 'hasChildren'}" @row-click="elementlink" :header-cell-style="{background:'rgba(150, 154, 146, 0.26)',color:'#606266'}" highlight-current-row @current-change="ResourceTableChange">
+            <el-table :data="ResourceTableData" style="width: 100%;margin-bottom: 20px;" height="1000px" row-key="资源名称" border :tree-props="{children: 'children', hasChildren: 'hasChildren'}" @row-click="elementlink" :header-cell-style="{background:'rgba(150, 154, 146, 0.26)',color:'#606266'}" highlight-current-row @current-change="ResourceTableChange" @row-contextmenu="rightClick">
                 <el-table-column prop="资源名称" label="资源名称">
                 </el-table-column>
                 <el-table-column prop="类型" label="类型">
                 </el-table-column>
             </el-table>
+            <div v-show="menuVisible">
+                <ul id="menu" class="menu">
+                    <li class="menu__item" @click="handleAdd()">本级新增</li>
+                    <li class="menu__item" @click="handleAdd">下级新增</li>
+                    <li class="menu__item">修改</li>
+                    <li class="menu__item">删除</li>
+                </ul>
+            </div>
         </el-aside>
         <!-- 要素目录表格 -->
         <el-main>
@@ -143,25 +151,47 @@
                 </el-carousel> -->
             </el-row>
             <el-row>
-                
-                <el-button type="text" @click="dialogFormVisible = true">打开嵌套表单的 Dialog</el-button>
 
-                <el-dialog title="收货地址" :visible.sync="dialogFormVisible">
-                    <el-form :model="form">
-                        <el-form-item label="活动名称" :label-width="formLabelWidth">
-                            <el-input v-model="form.name" autocomplete="off"></el-input>
-                        </el-form-item>
-                        <el-form-item label="活动区域" :label-width="formLabelWidth">
-                            <el-select v-model="form.region" placeholder="请选择活动区域">
-                                <el-option label="区域一" value="shanghai"></el-option>
+                <el-dialog title="新增资源" :visible.sync="dialogFormVisible" width="30%">
+                    <el-form ref="mform" :model="form" label-width="80px">
+                        <el-form-item label="上级">
+                            <el-select v-model="form.上级资源ID" placeholder="请选择上级">
+                                <el-option v-for="item in ResourceTableData" :label="item.资源名称" :value="item.资源ID"></el-option>
+                                
                             </el-select>
                         </el-form-item>
+                        <el-form-item label="类型">
+                            <el-select v-model="form.类型" placeholder="请选择资源类型">
+                                <el-option label="目录" value="目录"></el-option>
+                                <el-option label="资源" value="资源"></el-option>
+                                <el-option label="附加资源" value="附加资源"></el-option>
+                                <el-option label="资源关系" value="资源关系"></el-option>
+                                <el-option label="事务" value="事务"></el-option>
+                                <el-option label="业务" value="业务"></el-option>
+                                <el-option label="业务明细" value="业务明细"></el-option>
+                            </el-select>
+                        </el-form-item>
+                        <el-form-item label="名称">
+                            <el-input v-model="form.资源名称"></el-input>
+                        </el-form-item>
+                        <el-form-item label="显示名称">
+                            <el-input v-model="form.显示名称"></el-input>
+                        </el-form-item>
+
+                        <el-form-item label="是否树形">
+                            <el-switch v-model="form.是否树形" :active-value="1":inactive-value="0"@change=chang($event,state)></el-switch>
+                        </el-form-item>
+                        <el-form-item label="备注">
+
+                            <el-input type="textarea" v-model="form.备注"></el-input>
+                        </el-form-item>
                     </el-form>
-                    <div slot="footer" class="dialog-footer">
-                        <el-button @click="dialogFormVisible = false">取 消</el-button>
-                        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
-                    </div>
+                    <span slot="footer" class="dialog-footer">
+                        <el-button @click="dialogFormVisible=false">取 消</el-button>
+                        <el-button type="primary" @click="handleConfirm">确 定</el-button>
+                    </span>
                 </el-dialog>
+
             </el-row>
         </el-main>
     </el-container>
@@ -170,19 +200,34 @@
 
 <script>
 import axios from 'axios';
+import Vue from 'vue'
+import { Message } from 'element-ui'
+Vue.use(Message)
+Vue.prototype.$message = Message;
 export default {
     data() {
         return {
             ResourceTableData: [],
+            ALL:[],
             ElementTableData: [],
             ElementClassifyTableData: [],
             CountNormTableData: [],
             ElementRangeTableData: [],
             //标签页首选选项卡
             activeName: 'first',
-
+            menuVisible: false,
             dialogFormVisible: false,
-            form: [],
+            form:{
+                上级资源ID:"",
+                资源名称:"",
+                类型:"",
+                备注:"",
+                最后修改人:"",
+                显示名称:"",
+                是否树形:0,
+                关系资源ID:""
+
+            },
             formLabelWidth: '120px'
         };
     },
@@ -191,6 +236,7 @@ export default {
 
     },
     methods: {
+
         //资源目录树形网格
         treeList() {
             var that = this;
@@ -214,6 +260,7 @@ export default {
                     return arr;
                 }, []);
                 that.ResourceTableData = resultdata;
+                that.ALL=treeMapList;
                 console.log(that.ResourceTableData);
             })
         },
@@ -269,7 +316,68 @@ export default {
         },
         ElementRangeTableChange(val) {
             this.current = val;
+        },
+        //y j c d
+        rightClick(row, event) {
+            this.menuVisible = false; // 先把模态框关死，目的是 第二次或者第n次右键鼠标的时候 它默认的是true
+            this.menuVisible = true; // 显示模态窗口，跳出自定义菜单栏
+            var menu = document.querySelector('#menu');
+
+            this.styleMenu(menu);
+            console.log(row);
+            console.log(event);
+
+        },
+        foo() {
+            // 取消鼠标监听事件 菜单栏
+            this.menuVisible = false;
+            document.removeEventListener('click', this.foo); // 要及时关掉监听
+        },
+        styleMenu(menu) {
+            event.preventDefault();
+            if (event.clientX > 1800) {
+                menu.style.left = event.clientX - 100 + 'px';
+            } else {
+                menu.style.left = event.clientX + 1 + 'px';
+            }
+            document.addEventListener('click', this.foo); // 给整个document新增监听鼠标事件，点击任何位置执行foo方法
+            if (event.clientY > 700) {
+                menu.style.top = event.clientY - 30 + 'px';
+            } else {
+                menu.style.top = event.clientY - 10 + 'px';
+            }
+        },
+        //点击增加
+        handleAdd() {
+            this.dialogFormVisible = true;
+        },
+
+         resetForm(formName) {
+         this.$refs[formName].resetFields();},
+
+        //点击确认
+        handleConfirm() { 
+           
+    
+            this.$ajax.post('http://localhost:52225/api/Resource/GreateNResource',this.form)                  
+                //返回成功调用
+                .then((res)=>{
+                     this.$message('新增成功!');
+                     this.dialogFormVisible=false;
+                     this.$refs['mform'].resetFields(); // 清空表单
+                     this.$refs['mform'].clearValidate();//清空数据
+                     location.reload();
+
+                })
+                //返回失败调用
+                .catch((res)=>{
+                     this.$message('新增失败!');
+                     
+
+                
+                });
         }
+
     }
 }
 </script>
@@ -279,4 +387,26 @@ export default {
 .elcochoose {
     margin-top: 10px;
 }
-</style>>
+
+.menu__item {
+    display: block;
+    line-height: 20px;
+    text-align: left;
+    margin-top: 10px;
+
+}
+
+.menu {
+    height: 130px;
+    width: 70px;
+    position: absolute;
+    border-radius: 10px;
+    border: 1px solid #999999;
+    background-color: #f4f4f4;
+}
+
+li:hover {
+    background-color: #1790ff;
+    color: white;
+}
+</style>
