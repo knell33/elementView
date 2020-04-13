81a1352306f62591d3b94858d9912fde111d5620
TL-ai <tanglin_zhou@163.com>
2020-04-13 19:44:34 +0800

    4.13up2

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -13,7 +13,7 @@
         <!-- 要素目录表格 -->
         <el-main>
             <el-row>
-                <el-table :data="ElementTableData" height="460px" @row-click="elementrangelink" :header-cell-style="{background:'rgba(150, 154, 146, 0.26)',color:'#606266'}" border highlight-current-row @current-change="ElementTableChange">
+                <el-table :data="ElementTableData" height="460px" @row-click="elementrangelink" :header-cell-style="{background:'rgba(150, 154, 146, 0.26)',color:'#606266'}" border highlight-current-row @current-change="ElementTableChange" @row-contextmenu="elrightClick">
                     <el-table-column label="序号" prop="Numbera">
                     </el-table-column>
                     <el-table-column label="分类名称" prop="ClassifyName">
@@ -24,7 +24,7 @@
                     </el-table-column>
                     <el-table-column label="选项类型" prop="OptionType">
                     </el-table-column>
-                    <el-table-column label="定义指标个数" prop="DYZBGS">
+                    <el-table-column label="定义指标个数" prop="DYZBGS" width="110px">
                     </el-table-column>
                     <el-table-column label="单位" prop="Unit">
                     </el-table-column>
@@ -40,7 +40,7 @@
                     </el-table-column>
                     <el-table-column label="最后修改人" prop="LastModify">
                     </el-table-column>
-                    <el-table-column label="最后修改时间" prop="LastDate">
+                    <el-table-column label="最后修改时间" prop="LastDate" width="165px">
                     </el-table-column>
                 </el-table>
             </el-row>
@@ -146,15 +146,15 @@
             </el-row>
             <el-row>
                 <!-- 资源目录新增、修改弹框 -->
-                <el-dialog :title="choosetitle" :visible.sync="dialogFormVisible" :before-close="closedialog" width="30%" id="resourceiframe">
+                <el-dialog :title="choosetitle" :visible.sync="dialogFormVisible" :before-close="closedialog" width="30%">
                     <el-form ref="form" :model="form" status-icon label-width="80px">
                         <el-form-item label="上级" prop="PID">
-                            <el-select v-model="form.PID" filterable :filter-method="resourcedatafilter" :default-first-option=true placeholder="请选择上级">
+                            <el-select v-model="form.PID" filterable :filter-method="resourcedatafilter" :default-first-option=true placeholder="请选择上级" style="width:100%">
                                 <el-option v-for="(item,index) in PList" :label="item.Name" :value="item.ID" :key="index"></el-option>
                             </el-select>
                         </el-form-item>
                         <el-form-item label="类型" prop="Type">
-                            <el-select v-model="form.Type" placeholder="请选择资源类型">
+                            <el-select v-model="form.Type" placeholder="请选择资源类型" style="width:100%">
                                 <el-option label="目录" value="目录"></el-option>
                                 <el-option label="资源" value="资源"></el-option>
                                 <el-option label="附加资源" value="附加资源"></el-option>
@@ -183,6 +183,104 @@
                         <el-button type="primary" :loading="loading" @click="submitForm('form')">确 定</el-button>
                     </span>
                 </el-dialog>
+                <!-- 要素目录新增、修改弹框 -->
+                <el-dialog :title="elchoosetitle" :visible.sync="eldialogFormVisible" :before-close="closedialog" width="40%">
+                    <el-form ref="elform" :model="elform" status-icon label-width="110px" :inline="true" label-position="left">
+                        <el-form-item label="序号">
+                            <el-input v-model="elform.Numbera"></el-input>
+                        </el-form-item>
+                        <el-form-item label="资源" style="margin-left:20px">
+                            <el-select v-model="elform.RID" filterable :filter-method="resourcedatafilter" :default-first-option=true placeholder="请选择资源">
+                                <el-option v-for="(item,index) in PList" :label="item.Name" :value="item.ID" :key="index"></el-option>
+                            </el-select>
+                        </el-form-item>
+                        <el-form-item label="要素名称">
+                            <el-input v-model="elform.Name"></el-input>
+                        </el-form-item>
+                        <el-form-item label="分类" style="margin-left:20px">
+                            <el-select v-model="elform.CID" filterable :filter-method="resourcedatafilter" :default-first-option=true placeholder="请选择分类" style="width:217px">
+                                <el-option v-for="(item,index) in ClassifyList" :label="item.CName" :value="item.CID" :key="index"></el-option>
+                            </el-select>
+                        </el-form-item>
+                        <el-form-item label="要素类型">
+                            <el-select v-model="elform.Type" placeholder="请选择要素类型" style="width:203px">
+                                <el-option label="是否" value="是否"></el-option>
+                                <el-option label="数字" value="数字"></el-option>
+                                <el-option label="单行文本" value="单行文本"></el-option>
+                                <el-option label="单行文本(带选项)" value="单行文本(带选项)"></el-option>
+                                <el-option label="多行文本" value="多行文本"></el-option>
+                                <el-option label="日期" value="日期"></el-option>
+                                <el-option label="日期时间" value="日期时间"></el-option>
+                                <el-option label="选项" value="选项"></el-option>
+                                <el-option label="资源" value="资源"></el-option>
+                                <el-option label="通用编码" value="通用编码"></el-option>
+                                <el-option label="文件" value="文件"></el-option>
+                                <el-option label="链接" value="链接"></el-option>
+                            </el-select>
+                        </el-form-item>
+                        <el-form-item label="选项类型" style="margin-left:17px">
+                            <el-select v-model="elform.OptionType" placeholder="请选择选项类型" style="width:219px">
+                                <el-option label="下拉单选" value="下拉单选"></el-option>
+                                <el-option label="下拉多选" value="下拉多选"></el-option>
+                                <el-option label="平面单选" value="平面单选"></el-option>
+                                <el-option label="平面多选" value="平面多选"></el-option>
+                            </el-select>
+                        </el-form-item>
+                        <el-form-item label="值域资源">
+                            <el-select v-model="elform.DRID" filterable :filter-method="resourcedatafilter" :default-first-option=true placeholder="请选择资源" style="width:203px">
+                                <el-option v-for="(item,index) in DRList" :label="item.DRName" :value="item.DRID" :key="index"></el-option>
+                            </el-select>
+                        </el-form-item>
+                        <el-form-item label="是否必填" style="margin-left:17px">
+                            <el-select v-model="elform.IFRequired" placeholder="请选择" style="width:219px">
+                                <el-option label="是" value="1"></el-option>
+                                <el-option label="否" value="0"></el-option>
+                            </el-select>
+                        </el-form-item>
+                        <el-form-item label="长度">
+                            <el-input v-model="elform.Length"></el-input>
+                        </el-form-item>
+                        <el-form-item label="精度" style="margin-left:20px">
+                            <el-input v-model="elform.Precision" style="width:217px"></el-input>
+                        </el-form-item>
+                        <el-form-item label="单位">
+                            <el-input v-model="elform.Unit"></el-input>
+                        </el-form-item>
+                        <el-form-item label="默认值" style="margin-left:20px">
+                            <el-input v-model="elform.Defaulta" style="width:217px"></el-input>
+                        </el-form-item>
+                        <el-form-item label="是否展示主目录">
+                            <el-select v-model="elform.IFZSZML" placeholder="请选择" style="width:202px">
+                                <el-option label="是" value="1"></el-option>
+                                <el-option label="否" value="0"></el-option>
+                            </el-select>
+                        </el-form-item>
+                        <el-form-item label="是否展示名称" style="margin-left:17px">
+                            <el-select v-model="elform.IFZSMC" placeholder="请选择" style="width:219px">
+                                <el-option label="是" value="1"></el-option>
+                                <el-option label="否" value="0"></el-option>
+                            </el-select>
+                        </el-form-item>
+                        <el-form-item label="编码目录">
+                            <el-select v-model="elform.CDID" filterable :filter-method="resourcedatafilter" :default-first-option=true placeholder="请选择资源" style="width:202px">
+                                <el-option v-for="(item,index) in CDList" :label="item.CDName" :value="item.CDID" :key="index"></el-option>
+                            </el-select>
+                        </el-form-item>
+                        <el-form-item label="要素分类" style="margin-left:20px">
+                            <el-select v-model="elform.ElementClassify" placeholder="请选择" style="width:219px">
+                                <el-option label="普通要素" value="普通要素"></el-option>
+                                <el-option label="计算要素" value="计算要素"></el-option>
+                            </el-select>
+                        </el-form-item>
+                        <el-form-item label="备注">
+                            <el-input type="textarea" v-model="elform.Note" style="width:564px"></el-input>
+                        </el-form-item>
+                    </el-form>
+                    <span slot="footer" class="dialog-footer">
+                        <el-button @click="colse()">取 消</el-button>
+                        <el-button type="primary" :loading="loading" @click="elsubmitForm('elform')">确 定</el-button>
+                    </span>
+                </el-dialog>
             </el-row>
         </el-main>
     </el-container>
@@ -191,8 +289,16 @@
         <ul id="menu" class="menu">
             <li class="ms-item wrap-ms-right" @click="handleAdd()"><i class="el-icon-circle-plus icon1"></i>新增资源事务</li>
             <li class="ms-item wrap-ms-right" @click="handleXAdd()"><i class="el-icon-circle-plus icon1"></i>新增下级事务</li>
-            <li class="ms-item wrap-ms-right" @click="handleUpdate()"><i class="el-icon-s-order icon1"></i>修改</li>
-            <li class="ms-item wrap-ms-right" @click="handleDelete()"><i class="el-icon-delete-solid icon1"></i>删除</li>
+            <li class="ms-item wrap-ms-right" @click="handleUpdate()"><i class="el-icon-s-order icon1"></i>修改资源事务</li>
+            <li class="ms-item wrap-ms-right" @click="handleDelete()"><i class="el-icon-delete-solid icon1"></i>删除资源事务</li>
+        </ul>
+    </div>
+    <!-- 要素目录右键菜单 -->
+    <div v-show="elmenuVisible">
+        <ul id="elmenu" class="menu">
+            <li class="ms-item wrap-ms-right" @click="AddElement()"><i class="el-icon-circle-plus icon1"></i>新增要素</li>
+            <li class="ms-item wrap-ms-right" @click="UpdateElement()"><i class="el-icon-s-order icon1"></i>修改要素</li>
+            <li class="ms-item wrap-ms-right" @click="DeleteElement()"><i class="el-icon-delete-solid icon1"></i>删除要素</li>
         </ul>
     </div>
 </el-container>
@@ -200,6 +306,7 @@
 
 <script>
 import axios from 'axios';
+//import * as fecha from 'element-ui/lib/utils/date';
 export default {
     data() {
         return {
@@ -209,11 +316,13 @@ export default {
             ElementClassifyTableData: [],
             CountNormTableData: [],
             ElementRangeTableData: [],
+
             //标签页首选选项卡
             activeName: 'first',
-            //弹出框参数
+
+            //资源目录弹出框参数
             dialogFormVisible: false,
-            //资源目录弹框
+            //资源目录弹框表单
             form: {
                 ID: "",
                 PID: "",
@@ -232,18 +341,47 @@ export default {
             rid: null,
             //资源目录编辑标识
             mark: null,
-            //右键菜单模态框
-            menuVisible: false,
-
             loading: false,
-            //资源目录弹框
-            formLabelWidth: '120px',
+
+            //资源目录右键菜单模态框
+            menuVisible: false,
+            //要素目录右键菜单模态框
+            elmenuVisible: false,
+
             //资源目录对应操作的弹框标题
             choosetitle: '',
+            //要素目录对应操作的弹框标题
+            elchoosetitle: '',
+
             //资源目录上级select框自定义搜索方法
-             resourcedatafilter: null,
+            resourcedatafilter: null,
             //根据资源ID获取资源类型
             getType: '',
+
+            //要素目录修改时保存当前行的要素id
+            eid: '',
+            //要素目录弹窗表格
+            elform: {
+                EID: "",
+                RID: "",
+                Name: "",
+                Type: "",
+                Unit: "",
+                Note: "",
+                LastModify: "",
+                Length: '',
+                Precision: '',
+                Defaulta: "",
+                CID: "",
+                IFRequired: '0',
+                OptionType: "",
+                Numbera: "",
+                DRID: "",
+                IFZSZML: '0',
+                IFZSMC: '1',
+                CDID: "",
+                ElementClassify: "普通要素"
+            }
         };
     },
     created: function () {
@@ -256,7 +394,6 @@ export default {
             var obj = [];
             axios.post("GetAllResources").then(function (res) {
                 var Pdata = res.data;
-
                 for (let i in Pdata) {
                     obj.push({
                         ID: Pdata[i].ID,
@@ -265,6 +402,51 @@ export default {
                 }
                 //console.log("进入测试");
                 //console.log(obj);
+            });
+            return obj;
+        },
+        //要素目录分类列表
+        ClassifyList() {
+            var obj = [];
+            this.$ajax.post("GetAllElementClassifyByRID",
+                this.$qs.stringify({
+                    RID: this.rid
+                })).then(function (res) {
+                var Cdata = res.data;
+                for (let i in Cdata) {
+                    obj.push({
+                        CID: Cdata[i].ID,
+                        CName: Cdata[i].Name
+                    })
+                }
+            });
+            return obj;
+        },
+        //值域资源列表
+        DRList(){
+            var obj = [];
+            this.$ajax.post("GetAllDR",).then(function (res) {
+                var DRdata = res.data;
+                for (let i in DRdata) {
+                    obj.push({
+                        DRID: DRdata[i].RID,
+                        DRName: DRdata[i].Name
+                    })
+                }
+            });
+            return obj;
+        },
+        //通用编码目录列表
+        CDList(){
+            var obj = [];
+            this.$ajax.get("GetAllGCodedir",).then(function (res) {
+                var CDdata = res.data;
+                for (let i in CDdata) {
+                    obj.push({
+                        CDID: CDdata[i].ID,
+                        CDName: CDdata[i].Name
+                    })
+                }
             });
             return obj;
         }
@@ -391,11 +573,27 @@ export default {
             this.rid = row.ID;
             console.log(event);
         },
+        //要素目录右键点击事件
+        elrightClick(row, event) {
+            this.elmenuVisible = false; // 先把模态框关死，目的是 第二次或者第n次右键鼠标的时候 它默认的是true
+            this.elmenuVisible = true; // 显示模态窗口，跳出自定义菜单栏
+            var elmenu = document.querySelector('#elmenu');
+            this.styleMenu(elmenu);
+            this.srow = row; //将当前行的数据保存至srow中
+            // console.log("进入保存");
+            console.log(this.srow);
+            // console.log("退出保存");
+            this.rid = row.RID;
+            this.eid = row.EID;
+            // console.log(event);
+        },
         //弹窗公共方法
         foo() {
-            // 取消鼠标监听事件 菜单栏
+            //取消资源目录鼠标监听事件
             this.menuVisible = false;
-            document.removeEventListener('click', this.foo); // 要及时关掉监听
+            //取消要素目录鼠标监听事件
+            this.elmenuVisible = false;
+            document.removeEventListener('click', this.foo); //及时关掉监听
         },
         styleMenu(menu) {
             event.preventDefault();
@@ -414,7 +612,6 @@ export default {
 
         //资源目录新增本级
         handleAdd() {
-            // this.title = "新增本级资源目录";
             this.choosetitle = "新增资源事务";
             this.dialogFormVisible = true;
             this.mark = 1; //编辑标识
@@ -423,7 +620,6 @@ export default {
 
         //资源目录新增下级
         handleXAdd() {
-            // this.title = "新增下级资源目录"
             this.choosetitle = "新增下级事务";
             this.dialogFormVisible = true;
             this.mark = 1;
@@ -599,10 +795,15 @@ export default {
                 }
             }
         },
+
         //关闭并清空模态框
         colse() {
+            //关闭资源目录弹框
             this.dialogFormVisible = false;
             this.form = {};
+            //关闭要素目录弹框
+            this.eldialogFormVisible = false;
+            this.elform = {};
         },
         //点击X关闭模态框
         closedialog(done) {
@@ -615,11 +816,11 @@ export default {
                     done();
                     this.form = {};
                     this.mark = null;
-                    this.title = "";
                     this.$refs['form'].resetFields();
                 })
                 .catch(_ => {});
         },
+
         //资源目录右键删除
         handleDelete() {
             this.$confirm('是否要删除当前资源目录？', '提示', {
@@ -653,22 +854,40 @@ export default {
                 })
                 .catch(() => {});
         },
+        // 下拉框开启搜索功能
+        resourcedatafilter(val) {
+            this.value = val
+            if (val) {
+                this.options = this.optionsCopy.filter((item => {
+                    if (!!~item.label.indexOf(val) || !!~item.label.toUpperCase().indexOf(val.toUpperCase())) {
+                        return true;
+                    }
+                }))
+            } else {
+                this.options = this.optionsCopy; //val为空时，还原数组
+            }
+        },
 
-    },
-    // 上级资源列表下拉框开启搜索功能
-    resourcedatafilter(val) {
-        this.value = val
-        if (val) {
-            this.options = this.optionsCopy.filter((item => {
-                if (!!~item.label.indexOf(val) || !!~item.label.toUpperCase().indexOf(val.toUpperCase())) {
-                    return true;
-                }
-            }))
-        } else {
-            this.options = this.optionsCopy; //val为空时，还原数组
-        }
-    },
+        //列表日期格式转换
+        // dateFormat(row,column,cellValue){
+        //     console.log(row,column,cellValue);
+        //     return cellValue?fecha.format(new Date(cellValue),'yyyy-MM-dd'):'';
+        // },
+        //要素目录新增
+        AddElement() {
+            this.elchoosetitle = "新增要素";
+            this.eldialogFormVisible = true;
+            this.mark = 1;
+        },
+        //要素目录修改
+        UpdateElement() {
 
+        },
+        //要素目录删除
+        DeleteElement() {
+
+        },
+    }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -3,15 +3,14 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+//表格日期显示格式转换
+import * as fecha from "element-ui/lib/utils/date"
 //导入全局样式表
 import './assets/css/global.css'
 //导入ajax
 import axios from 'axios';
 //qs处理ajaxJSON参数
 import qs from 'qs';
-//引入layui
-
-
 
 Vue.config.productionTip = false
 
@@ -20,6 +19,7 @@ Vue.config.productionTip = false
 //     render: h => h(App)
 // }).$mount('#app')
 Vue.use(ElementUI);
+Vue.use(fecha);
 
 //配置请求根路径
 axios.defaults.baseURL = 'http://localhost:62200/api/'
